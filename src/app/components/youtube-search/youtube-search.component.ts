7f00e8f414c767a82b3dfdc6d0edfe8a9765c4af
Daniel Baum <daniel.m.baum@gmail.com>
2019-08-14 22:03:51 -0400

    Adding form validation

--- src/app/components/youtube-search/youtube-search.component.ts
+++ src/app/components/youtube-search/youtube-search.component.ts
@@ -1,10 +1,9 @@
 import { Component, Injectable, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { YoutubeService } from '../../services/youtube.service';
-import { FormGroup, FormControl } from '@angular/forms';
-import { LocalStorage } from '@ngx-pwa/local-storage';
 
 import * as unescape from 'unescape';
 import { Video } from 'src/app/model/video';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
 
 @Component({
     selector: 'app-youtube-search',
@@ -15,29 +14,14 @@ import { Video } from 'src/app/model/video';
 @Injectable()
 export class YoutubeSearchComponent implements OnInit {
 
-    searchForm = new FormGroup({
-        title: new FormControl(''),
-    });
-    searching = false;
-    title: string;
-    videos: Video[];
+    searching: boolean = false;
+    videos: Array<Video>;
     informed: number;
     pageToken: string = null;
 
-    constructor(private youtubeService: YoutubeService,
-                protected localStorage: LocalStorage) { }
+    constructor(private youtubeService: YoutubeService) { }
 
-    ngOnInit() {
-        this.localStorage.getItem('videos').subscribe((videos: Video[]) => {
-          this.videos = videos;
-        });
-        this.localStorage.getItem<string>('title').subscribe((title: string) => {
-          this.title = title;
-        });
-        this.localStorage.getItem<string>('pageToken').subscribe((pageToken: string) => {
-          this.pageToken = pageToken;
-        });
-    }
+    ngOnInit() { }
 
     /**
      * search
@@ -47,39 +31,26 @@ export class YoutubeSearchComponent implements OnInit {
             console.warn('Cannot start a new serach while current search is still gathering info on the videos');
             return;
         }
-        if (!more && (!this.searchForm.value.title || this.searchForm.value.title.length === 0)) {
-            alert('Must enter a title');
-            return;
-        }
         this.searching = true;
-        if (!more ) {
-            this.title = this.searchForm.value.title;
-        }
-        this.searchForm.value.title = '';
         if (!more) {
-          this.localStorage.setItem('title', this.title).subscribe(() => {});
           this.pageToken = null;
           this.videos = [];
         }
-        this.youtubeService.search(this.title, this.pageToken).subscribe( result => {
+        this.youtubeService.search(this.titleValue, this.pageToken).subscribe( result => {
             this.pageToken = result['nextPageToken'];
             if (more) {
                 for (const video of result['items']) {
-                  video.snippet.title = unescape(video.snippet.title);
-                  video.ext = 'mp4';
                   this.videos.push(video);
                 }
             } else {
                 this.videos = result['items'];
-                for (const video of this.videos) {
-                  video.snippet.title = unescape(video.snippet.title);
-                  video.ext = 'mp4';
-                }
             }
-            this.localStorage.setItem('videos', this.videos).subscribe(() => {});
-            this.localStorage.setItem('pageToken', this.pageToken).subscribe(() => {});
+            for (const video of this.videos) {
+              video.snippet.title = unescape(video.snippet.title);
+              video.ext = 'mp4';
+            }
             this.searching = false;
-        }, error => {
+        }, error => { // Handle error
             console.error(error);
             this.searching = false;
         });
@@ -93,15 +64,8 @@ export class YoutubeSearchComponent implements OnInit {
             const found = this.videos.filter(x => x.id.videoId === video['id'])[0];
             found.formats = video['formats'];
             found.ext = video['ext'];
-        // Handle error
-        }, error => {
+        }, error => { // Handle error
             console.error(error);
-            this.informed++;
-            if (this.informed === this.videos.length) { this.searching = false; }
-        // Finally do this
-        }, () => {
-            this.informed++;
-            if (this.informed === this.videos.length) { this.searching = false; }
         });
     }
 }
